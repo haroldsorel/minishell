4e0ed38516460a4979d90aad01f9f6137e96d12b
Harold Sorel <133993985+haroldsorel@users.noreply.github.com>
2024-11-20 22:13:50 +0100

    cleaned

--- misc/syntax_checker.c
+++ misc/syntax_checker.c
@@ -11,41 +11,6 @@
 /* ************************************************************************** */
 #include "minishell.h"
 
-int syntax_error_handler(char *str)
-{
-    printf("Syntax Error : %s\n", str); //PRINTF REPLACE  and just put str
-    return (-1);
-}
-
-int quotes_skipper(char *input, int *i)
-{
-    if (input[*i] == '"')
-    {
-        (*i)++;
-        while (input[*i] != '"')
-        {
-            if (input[*i] == '\0')
-                return (-1);
-            (*i)++;
-        }
-        (*i)++;
-        return (1);
-    }
-    if (input[*i] == '\'')
-    {
-        (*i)++;
-        while (input[*i] != '\'')
-        {
-            if (input[*i] == '\0')
-                return (-1);
-            (*i)++;
-        }
-        (*i)++;
-        return (1);
-    }
-    return (0);
-}
-
 int quotes_checker(char *input)
 {
     int i;
